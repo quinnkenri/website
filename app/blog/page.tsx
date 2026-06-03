a03872006dfb286740db04e31160eaf2dd0d7e36
Quinn Lawson <138352382+quinnkenri@users.noreply.github.com>
2026-06-02 20:23:01 -0600

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,8 @@
 import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
+  title: 'Projects',
+  description: 'check out my projects',
 }
 
 export default function Page() {
