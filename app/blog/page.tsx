262f2ce7c1f8dca71ac302d6fc15f9356238dbd4
Quinn Lawson <quinn.lawson@usask.ca>
2026-06-02 21:50:36 -0600

    Revert "Refactor Blog page to async functions"
    This reverts commit 9cb4a88843031a4160496ca8f37e54452062c10d.

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,13 +1,15 @@
 import { BlogPosts } from 'app/components/posts'
 
-export async function generateMetadata({ params }) {
-  const { slug } = await params
-  let post = getBlogPosts().find((post) => post.slug === slug)
-  ...
+export const metadata = {
+  title: 'Projects',
+  description: 'check out my projects',
 }
 
-export default async function Blog({ params }) {
-  const { slug } = await params
-  let post = getBlogPosts().find((post) => post.slug === slug)
-  ...
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <BlogPosts />
+    </section>
+  )
 }
